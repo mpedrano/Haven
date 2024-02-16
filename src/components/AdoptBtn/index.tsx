0b4302d9380d401473a49fdd7ab4467728e9e4ce
omnomrose <maryrosenguyenbusiness@gmail.com>
2024-02-16 00:53:07 -0800

    init, added daisyui

---
+++ src/components/AdoptBtn/index.tsx
@@ -0,0 +1,10 @@
+import styles from '@/components/AdoptBtn/AdoptBtn.module.css'
+
+export default function AdoptBtn(){
+
+    return(
+        <div>
+            <button className={styles.btn}>Adopt Today →</button>
+        </div>
+    )
+}
