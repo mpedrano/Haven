6e31066f20aca925e5518898e6b487cd235f2d0b
omnomrose <maryrosenguyenbusiness@gmail.com>
2024-03-16 21:44:09 -0500

    step 2 finish

---
+++ data/questions.tsx
@@ -0,0 +1,22 @@
+export const questions = [
+    [
+        "First Name",
+        "Last Name",
+        "Email",
+        "Phone Number",
+        "Address",
+        "City",
+        "Province/State",
+        "Postal Code"
+    ],
+    [
+        "Name of the Animal",
+        "Animal ID #",
+        "Sex",
+        "Birthday",
+        "Type", 
+        "Who will have responsibility for this pet?",
+        "Are there any other pets in this household?",
+        "If yes, please list: Name, Type, Age, Sex"
+    ],
+];
