5e26abc31ffd2576fd904160d8d961ca525f5bca
jennydmcm <113193732+jennydmcm@users.noreply.github.com>
2024-03-21 08:56:55 -0700

    fix

--- src/pages/cats.tsx
+++ src/pages/cats.tsx
@@ -1,6 +1,5 @@
 
 import styles from '@/styles/Cats.module.css'
-import FeatPetCard from '@/components/featPetCard'
 import CatPetCard from '@/components/CatPetCard'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
