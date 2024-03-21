5a32e4ac7031143a1e16dbdd72dc2b8c1314f0f2
jennydmcm <113193732+jennydmcm@users.noreply.github.com>
2024-03-21 13:33:10 -0700

    cats page edited

--- src/pages/cats.tsx
+++ src/pages/cats.tsx
@@ -1,17 +1,33 @@
-
-import styles from '@/styles/Cats.module.css'
-import CatPetCard from '@/components/CatPetCard'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+// Cats.tsx
+import React, { useEffect, useState } from 'react';
+import styles from '@/styles/Cats.module.css';
+import CatPetCard from '@/components/CatPetCard';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import axios from 'axios';
 
 export default function Cats() {
+    const [breeds, setBreeds] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fetchBreeds = async () => {
+            try {
+                const response = await axios.get('https://api.petfinder.com/v2/types/cat/breeds');
+                const breedList = response.data.breeds.map((breed: any) => breed.name);
+                setBreeds(breedList);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchBreeds();
+    }, []);
+
     return (
         <>
             <div className={styles.header}>
                 <Header />
             </div>
             <main className={styles.main}>
-
                 <div>
                     <h1 className={styles.title}>Your Feline Friend is Awaiting</h1>
                     <div className={styles.filters}>
@@ -20,24 +36,17 @@ export default function Cats() {
                             <button className={styles.allCats}>All Cats</button>
                             <button className={styles.other}>Other</button>
                         </div>
-                        <div >
-                            <select className={styles.dropdown} name="cats" id="cats">
-                                <option value="ragdoll">Ragdoll</option>
-                                <option value="siamese">Siamese</option>
-                                <option value="persian">Persian</option>
-                                <option value="shorthair">Shorthair</option>
-                            </select>
-                        </div>
+
                     </div>
                 </div>
-                <div>
+
+                <div className={styles.petCards}>
                     <CatPetCard />
                 </div>
-
             </main>
             <div className={styles.footer}>
                 <Footer />
             </div>
-        </>)
+        </>
+    );
 }
-
