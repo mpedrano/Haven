c0473fea7748ea635e6cc87ebe3350687af32884
linaaa <113660013+carolinalxu@users.noreply.github.com>
2024-03-21 13:57:09 -0700

    beginning animal info page

--- src/pages/animals/[id].tsx
+++ src/pages/animals/[id].tsx
@@ -1,14 +1,31 @@
 import { useRouter } from 'next/router';
 import AnimalInfoPage from '../../components/AnimalInfo';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Animal() {
   const router = useRouter();
   const { id } = router.query;
+  const [animal, setAnimal] = useState(null);
 
+  useEffect(() => {
+    const fetchAnimal = async () => {
+      try {
+        const response = await axios.get(`https://api.petfinder.com/v2/animals/${id}`);
+        setAnimal(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (id) {
+      fetchAnimal();
+    }
+  }, [id]);
 
   return (
     <div>
-      <AnimalInfoPage animal={} />
+      {animal && <AnimalInfoPage animal={animal} />}
     </div>
   );
 }
