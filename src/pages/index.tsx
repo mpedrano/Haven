31e1bf547160ea4b4b8168ef8041a67667b89170
linaaa <113660013+carolinalxu@users.noreply.github.com>
2024-03-21 15:53:47 -0700

    adding tablet styles incomplete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from '@/styles/Home.module.css'
 import SeeMoreBtn from "@/components/seeMoreBtn";
-
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import FeatResourceCard from "@/components/FeatResourceCard";
