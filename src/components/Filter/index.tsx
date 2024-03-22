1c40d1a136e40efce74afd456d1574f669343354
mpedrano <mpedrano@my.bcit.ca>
2024-03-22 11:53:42 -0700

    add filter enums

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import CatIcon from '../../../public/filter/caticon.svg'
 import DogIcon from '../../../public/filter/dogicon.svg'
 import OtherIcon from '../../../public/filter/othericon.svg'
+import { useRouter } from 'next/router'
 
 export enum Filter {
     Dog = "All Dogs",
@@ -15,23 +16,29 @@ type FilterIconProps = {
 }
 
 const FilterIcon = ({ filter }: FilterIconProps) => {
+    const router = useRouter();
     let icon: string;
+    let path: string;
 
     switch (filter) {
         case Filter.Dog:
             icon = DogIcon
+            path = '/dogs'
             break
         case Filter.Cat:
             icon = CatIcon
+            path = '/cats'
             break
         case Filter.Other:
             icon = OtherIcon
+            path = '/otherpets'
             break
         default:
             icon = 'unknown'
+            path = '/'
     }
     return (
-        <button className={styles.button}>
+        <button className={styles.button} onClick={() => router.push(path)}>
             <Image src={icon} alt="filter icon" width={30} height={30} />
             <div>{filter}</div>
         </button>
